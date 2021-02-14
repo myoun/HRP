1fd759914c8840b00e5b12b0edf8685b50a52549
myoung2namur <aiden080605@gmail.com>
2021-02-14 22:11:43 +0900

    TODO

--- src/main/kotlin/kr/myoung2/hrp/HRP.kt
+++ src/main/kotlin/kr/myoung2/hrp/HRP.kt
@@ -2,14 +2,22 @@ package kr.myoung2.hrp
 
 import kr.myoung2.hrp.commands.HRCommand
 import org.bukkit.plugin.java.JavaPlugin
-import org.projecttl.plugin.hrp.TestClass
 
 class HRP : JavaPlugin(){
 
     override fun onEnable() {
         getCommand("HR")?.setExecutor(HRCommand())
         getCommand("hr")?.setExecutor(HRCommand())
-
         super.onEnable()
+
+        TODO("Create Package and Make Event Listener")
+
+        /* TODO
+            InventoryClickEvent
+            Cancel Event
+            자세한 내용은 kr.myoung2.hrp.commands.HRCommand 를 확인하세요
+         */
+
+
     }
 }
--- src/main/kotlin/kr/myoung2/hrp/commands/HRCommand.kt
+++ src/main/kotlin/kr/myoung2/hrp/commands/HRCommand.kt
@@ -7,7 +7,6 @@ import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
-import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import org.projecttl.api.inventorygui.utils.CreateGUI
 
