d05689ea172a349e9984410c45f7fba1aca04293
myoung2namur <aiden080605@gmail.com>
2021-02-14 20:26:00 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("jvm") version "1.4.30"
+}
+
+group = "kr.myoung2"
+version = "1.0.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    compileOnly("com.github.ProjectTL12345:InventoryGUI:2.0.0")
+}
+
+val shade = configurations.create("shade")
+shade.extendsFrom(configurations.implementation.get())
+
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    processResources {
+        filesMatching("*.yml") {
+            expand(project.properties)
+        }
+    }
+
+    create<Jar>("sourceJar") {
+        archiveClassifier.set("source")
+        from(sourceSets["main"].allSource)
+    }
+
+    jar {
+        from (shade.map { if (it.isDirectory) it else zipTree(it) })
+    }
+}
---
+++ src/main/kotlin/kr/myoung2/hrp/HRP.kt
@@ -0,0 +1,10 @@
+package kr.myoung2.hrp
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HRP : JavaPlugin(){
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+}
