14d7a4f33e018d303b8348ff8ca36e754378fd33
Project_TL <kimpiao020@gmail.com>
2021-02-14 22:42:04 +0900

    fix: Project_TL(Error command fix)

--- src/main/kotlin/kr/myoung2/hrp/commands/HRCommand.kt
+++ src/main/kotlin/kr/myoung2/hrp/commands/HRCommand.kt
@@ -12,21 +12,33 @@ import org.projecttl.api.inventorygui.utils.CreateGUI
 
 class HRCommand : CommandExecutor {
 
+    val diamondSword: String = "${ChatColor.AQUA}날카로운 검"
+    val ironChestPlate: String = "${ChatColor.AQUA}단단한 갑옷"
+    val goldenPickaxe: String = "${ChatColor.GOLD}빠른 곡괭이"
+    val ironChestPlate2: String = "${ChatColor.AQUA}단단해진 갑옷"
+
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (!(sender is Player)) return false
-        val HRGUI:CreateGUI = CreateGUI(27, "${ChatColor.RED}인간 ${ChatColor.BLUE}강화").let {
+        if (sender !is Player) {
+            return false
+        } else {
+            if (command.name == "hr") {
+                val hrgUI: CreateGUI = CreateGUI(27, "${ChatColor.RED}인간 ${ChatColor.BLUE}강화").let {
 
-            it.setItem(ItemStack(Material.DIAMOND_SWORD),"${ChatColor.AQUA}날카로운 검",10)
-            it.setItem(ItemStack(Material.IRON_CHESTPLATE),"${ChatColor.AQUA}단단한 갑옷",12)
-            it.setItem(ItemStack(Material.GOLDEN_PICKAXE),"${ChatColor.GOLD}빠른 곡괭이",14)
-            it.setItem(ItemStack(Material.IRON_CHESTPLATE),"${ChatColor.AQUA}단단해진 갑옷",16)
+                    it.setItem(ItemStack(Material.DIAMOND_SWORD), "${ChatColor.AQUA}날카로운 검", 10)
+                    it.setItem(ItemStack(Material.IRON_CHESTPLATE), "${ChatColor.AQUA}단단한 갑옷", 12)
+                    it.setItem(ItemStack(Material.GOLDEN_PICKAXE), "${ChatColor.GOLD}빠른 곡괭이", 14)
+                    it.setItem(ItemStack(Material.IRON_CHESTPLATE), "${ChatColor.AQUA}단단해진 갑옷", 16)
 
-            it.setExitButton(22)
+                    it.setExitButton(22)
 
-            it
+                    it
+                }
+
+                hrgUI.openInventory(sender)
+                return true
+            }
         }
 
-        HRGUI.openInventory(sender)
-        return true
+        return false
     }
 }
