91face0a68ae664e78fd81c3405f853beb52c51e
myoung2namur <aiden080605@gmail.com>
2021-02-15 14:25:33 +0900

    Update HarderArmorListener.kt

--- src/main/kotlin/kr/myoung2/hrp/listeners/HarderArmorListener.kt
+++ src/main/kotlin/kr/myoung2/hrp/listeners/HarderArmorListener.kt
@@ -5,19 +5,18 @@ import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Item
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityPickupItemEvent
+import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 
 class HarderArmorListener(val plugin: HRP) : Listener {
 
     @EventHandler
-    fun onPlayerEquipItemByInventory(e:EntityPickupItemEvent) {
+    fun onPlayerGetArmor(e:EntityPickupItemEvent) {
         if (e.entityType != EntityType.PLAYER) return
-        val p:Player = e.entity as Player
 
         val item:Item = e.item
         val stack:ItemStack = item.itemStack
@@ -36,6 +35,24 @@ class HarderArmorListener(val plugin: HRP) : Listener {
         }
 
         meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL,enchantLevel,false)
+        stack.itemMeta = meta
+        item.itemStack = stack
+    }
+
+    @EventHandler
+    fun onPlayerDropArmor(e:PlayerDropItemEvent){
+
+        val item:Item = e.itemDrop
+        val stack:ItemStack = item.itemStack
+        val meta:ItemMeta = stack.itemMeta
+        val enchantLevel:Int = plugin.hrpConfig().getInt("ProtectionEnchantLevel")
+
+        if (!checkArmor(stack)) return
+
+        if (meta.enchants.containsKey(Enchantment.PROTECTION_ENVIRONMENTAL)){
+            if (meta.getEnchantLevel(Enchantment.PROTECTION_ENVIRONMENTAL) >= enchantLevel) return
+            else meta.removeEnchant(Enchantment.PROTECTION_ENVIRONMENTAL)
+        }
         stack.itemMeta = meta
         item.itemStack = stack
     }
